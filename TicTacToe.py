3bd259225d6b678d63305fa9085fbad904492245
Lucas Amaral <lucasamaral1@gmail.com>
2013-09-20 15:29:45 -0300

    added alfa beta cutoff bur not working yet

--- TicTacToe.py
+++ TicTacToe.py
@@ -1,4 +1,5 @@
 import time
+import sys
 
 class TicTacToe(object):
     def __init__(self):
@@ -32,7 +33,6 @@ class TicTacToe(object):
                             return (i,j,-1)
                     analisis = self.minimax_move(self.other_player(player))
                     path_results.append((i,j,analisis[2]))
-                    # self.print_board()
                     self.board[i][j] = ' '
         path_results = sorted(path_results, key=lambda x:x[2], reverse = True)
         if player == self.current_player:
@@ -40,6 +40,50 @@ class TicTacToe(object):
         else:
             return path_results[-1]
 
+    def alfa_beta_minimax(self, player, alfa, beta):
+        print '<<<<<>>>>'
+        print 'table_full: ' + str(self.table_full(self.board))
+        self.print_board()
+        if self.table_full(self.board):
+            return (-1,-1,0)
+
+        cur_beta_node = (-1,-1,sys.maxint)
+        cur_alfa_node = (-1,-1,-sys.maxint -1)
+
+        if not player == self.current_player:
+            for i in xrange(0,3):
+                for j in xrange(0,3):
+                    if self.board[i][j] == ' ':
+                        result = self.do_move(i,j,player)
+                        if result:
+                            self.board[i][j] = ' '
+                            return (i,j,-1)
+                        analisis = self.alfa_beta_minimax(self.other_player(player), alfa, beta) #min(beta, cur_beta_node[2])
+                        if cur_beta_node[2] <= analisis[2]:
+                            cur_beta_node = (i,j,cur_beta_node[2])
+                        else:
+                            cur_beta_node = analisis
+                        self.board[i][j] = ' '
+                        if alfa >= cur_beta_node[2]:
+                            return cur_beta_node
+            return cur_beta_node
+        else:
+            for i in xrange(0,3):
+                for j in xrange(0,3):
+                    if self.board[i][j] == ' ':
+                        result = self.do_move(i,j,player)
+                        if result:
+                            self.board[i][j] = ' '
+                            return (i,j,1)
+                        analisis = self.alfa_beta_minimax(self.other_player(player), alfa, beta) #max(alfa,cur_alfa_node[2])
+                        if cur_alfa_node[2] >= analisis[2]:
+                            cur_alfa_node = (i,j,cur_alfa_node[2])
+                        else:
+                            cur_alfa_node = analisis
+                        self.board[i][j] = ' '
+                        if cur_alfa_node[2] >= beta:
+                            return (i,j,cur_alfa_node[2])
+            return cur_alfa_node
 
     def human_move(self):
         print('Digite sua jogada na forma linha,coluna: ')
@@ -100,7 +144,9 @@ def play_game():
         result = game.human_move()
         game.print_board()
         game.current_player = game.other_player(game.current_player)
-        play = game.minimax_move(game.current_player)
+        # play = game.minimax_move(game.current_player)
+        play = game.alfa_beta_minimax(game.current_player, -sys.maxint-1, sys.maxint)
+        print 'jogada:' + str(play)
         if play[0] == -1:
             break;
         result = game.do_move(play[0], play[1], game.current_player)
